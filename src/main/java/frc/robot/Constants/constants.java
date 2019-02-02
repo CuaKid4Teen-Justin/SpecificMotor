98ba32eeed5af5ec069277ce4e6700259b3e4831
CuaKid4Teen <justin.cua14@gmail.com>
2019-02-01 18:38:18 -0800

    Thanks, Marcos

---
+++ src/main/java/frc/robot/Constants/constants.java
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.Constants;
+
+/**
+ * Add your docs here.
+ */
+public class constants {
+    // kp * ep + kD * eD
+  public static final double kP = 0.01;
+  public static final double tolerance = 3;
+  public static final double kD = 0;
+
+}
--- src/main/java/frc/robot/commands/TurnDegrees.java
+++ src/main/java/frc/robot/commands/TurnDegrees.java
@@ -9,15 +9,25 @@ package frc.robot.commands;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import frc.robot.Constants.constants;
 import frc.robot.subsystems.Drive;
 
 public class TurnDegrees extends Command {
 
+// Thanks, Marcos
+
   public double m_output;
   public double m_desiredDistance;
-  public double m_error;
+  public double m_currentError;
+
+  public double m_deritive;
+  public double m_oldTime;
+  public double m_currentTime;
+  public double m_oldError;
+
 
   //error = desired - actual distance
   //m_output 
@@ -25,8 +35,10 @@ public class TurnDegrees extends Command {
   //figure set power to motor^^
   // kP  = constant YOU trial and error tune
 
-  public TurnDegrees() {
+  public TurnDegrees(double distance) {
+    m_desiredDistance = distance; 
   requires(Robot.m_drive);
+
 
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
@@ -35,22 +47,40 @@ public class TurnDegrees extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    m_oldTime = Timer.getFPGATimestamp();
+    m_oldError= m_desiredDistance;
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    m_currentTime = Timer.getFPGATimestamp();
+// used for more precise calculations
+    m_currentError = m_desiredDistance - Robot.m_drive.Position();
+// ^^ tells how much further the Robot goes
+    m_deritive = (m_currentError - m_oldError) / (m_currentTime - m_oldTime);
+// ^^ PID formula stuff. The change of error / change of time to be more precise
+    m_output = m_currentError * constants.kP;
+// tells how much power the motor will change
+    Robot.m_drive.SetPower(m_output);
+// changes motor power
+
+    m_oldError = m_currentError;
+// how error distance updates so it won't repeat old mistake
+    m_oldTime = m_currentTime;
+// same as line above
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return Math.abs(m_currentError) < constants.tolerance;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.m_drive.SetPower(0);
   }
 
   // Called when another command which requires one or more of the same
