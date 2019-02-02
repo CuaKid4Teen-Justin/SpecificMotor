2e31a410ef2edae1e3400d30135dcd5be01f383d
CuaKid4Teen <justin.cua14@gmail.com>
2019-02-01 17:52:59 -0800

    initial commit

---
+++ src/main/java/frc/robot/commands/TurnDegrees.java
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.Drive;
+
+public class TurnDegrees extends Command {
+
+  public double m_output;
+  public double m_desiredDistance;
+  public double m_error;
+
+  //error = desired - actual distance
+  //m_output 
+  // drive subsystem does GetPosition
+  //figure set power to motor^^
+  // kP  = constant YOU trial and error tune
+
+  public TurnDegrees() {
+  requires(Robot.m_drive);
+
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
